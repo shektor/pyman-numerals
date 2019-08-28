ddbcd18013b00e073cbbf8bbfea83be1dad502a3
shektor <s.hektor@gmail.com>
2019-08-28 20:32:05 +0100

    Add basic convert logic
    To handle numbers from 1 to 8 the convert method will need to know the 
    numerals for 1 and 5. The method will then check if the number is above 
    or below 5 to build the numeral string.

--- roman_numeral.py
+++ roman_numeral.py
@@ -1,3 +1,15 @@
 
 def convert(number):
-    return 'I'
+    numerals = {
+        1: 'I',
+        5: 'V',
+    }
+
+    if number < 5:
+        if number < 4:
+            return numerals[1] * number
+        else:
+            return numerals[1] + numerals[5]
+
+    value_over_five = number % 5
+    return numerals[5] + (numerals[1] * value_over_five)
--- roman_numeral_test.py
+++ roman_numeral_test.py
@@ -1,8 +1,16 @@
+import pytest
 from roman_numeral import convert
 
 
-def test_convert():
-    number = 1
-    numeral = 'I'
-
+@pytest.mark.parametrize('number, numeral', [
+    (1, 'I'),
+    (2, 'II'),
+    (3, 'III'),
+    (4, 'IV'),
+    (5, 'V'),
+    (6, 'VI'),
+    (7, 'VII'),
+    (8, 'VIII'),
+])
+def test_convert(number, numeral):
     assert convert(number) == numeral
