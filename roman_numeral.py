2d96697745cb0e6fde9750dfcc4002b2882f913e
shektor <s.hektor@gmail.com>
2019-08-29 11:55:08 +0100

    Update convert to use additive notation
    A simpler form of roman numerals is additive notation where the unit 
    value of 4 employs the symbol 'IIII' instead of 'IV'.
    
    Use a unit list with values in descending order to loop through, check 
    if the number arguement is larger than the unit, add the numeral symbol 
    to a string and subtract the unit from the number. Repeat this until 
    number is 0.

--- roman_numeral.py
+++ roman_numeral.py
@@ -4,12 +4,14 @@ def convert(number):
         1: 'I',
         5: 'V',
     }
+    units = [5, 1]
 
-    if number < 5:
-        if number < 4:
-            return numerals[1] * number
-        else:
-            return numerals[1] + numerals[5]
+    numeral = ''
+    while number > 0:
+        for unit in units:
+            if number >= unit:
+                numeral += numerals[unit]
+                number -= unit
+                break
 
-    value_over_five = number % 5
-    return numerals[5] + (numerals[1] * value_over_five)
+    return numeral
--- roman_numeral_test.py
+++ roman_numeral_test.py
@@ -2,15 +2,29 @@ import pytest
 from roman_numeral import convert
 
 
+# @pytest.mark.parametrize('number, numeral', [
+#     (1, 'I'),
+#     (2, 'II'),
+#     (3, 'III'),
+#     (4, 'IV'),
+#     (5, 'V'),
+#     (6, 'VI'),
+#     (7, 'VII'),
+#     (8, 'VIII'),
+# ])
+# def test_convert(number, numeral):
+#     assert convert(number) == numeral
+
+
 @pytest.mark.parametrize('number, numeral', [
     (1, 'I'),
     (2, 'II'),
     (3, 'III'),
-    (4, 'IV'),
+    (4, 'IIII'),
     (5, 'V'),
     (6, 'VI'),
     (7, 'VII'),
     (8, 'VIII'),
 ])
-def test_convert(number, numeral):
+def test_convert_additive(number, numeral):
     assert convert(number) == numeral
